b18f429a021ac4dde4e840871d4bae378e61afa5
Gsd.Lingga <gusdeklingga@yahoo.co.id>
2022-06-11 17:00:28 +0800

    feat: count emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+(promiseOutput = async (emosi) => {
+  try {
+    const theatherIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+
+    const combineData = [...theatherIXX, ...theaterVGC];
+
+    const filterData = combineData.filter((element) => element.hasil === emosi);
+
+    return filterData.length;
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
 module.exports = {
   promiseOutput,
